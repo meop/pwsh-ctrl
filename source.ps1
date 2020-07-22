a86a7e98ed0a5c47f9c44dd80a7302ffff789558
Marshall Porter <meoporter@gmail.com>
2020-07-21 23:01:39 -0700

    move modules to folder

--- source.ps1
+++ source.ps1
@@ -1,14 +1,12 @@
-Import-Module "$PSScriptRoot/setups.psm1"
+$d = "$PSScriptRoot/modules"
+if (Test-Path $d) {
+    Get-ChildItem -Path $d -Filter '*.psm1' |
+    ForEach-Object { Import-Module $_.FullName }
+}
 
 Set-Alias -Name setups -Value Update-Setups
-
-Import-Module "$PSScriptRoot/clones.psm1"
-
 Set-Alias -Name backups -Value Invoke-Backups
 Set-Alias -Name dotfiles -Value Invoke-DotFiles
-
-Import-Module "$PSScriptRoot/packages.psm1"
-
 Set-Alias -Name packages -Value Invoke-Packages
 
 $global:SETUPS_ASSETS_DIR = "$PSScriptRoot/assets"
